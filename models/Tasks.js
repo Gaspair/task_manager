b5f091b24ccbb7b324fb31179df51a983b551e20
DRAGOS BALUTA <DIULIANB16@GMAIL.COM>
2023-05-19 17:28:52 +0300

    -add comments functionality

--- models/Tasks.js
+++ models/Tasks.js
@@ -7,6 +7,12 @@ const TaskSchema = new mongoose.Schema({
     trim: true,
     maxlength: [20, "name can not be more than 20 characters"],
   },
+  comment: {
+    type: String,
+    requires: [true, "please provide comments or updates on the task!"],
+    maxlength: [200, "comment cannot be longer than 200 characters"],
+    default: "",
+  },
   completed: {
     type: Boolean,
     default: false,
